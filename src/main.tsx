338462fb6ac7304de29a9977eaa9c78fd1e289b7
mag123c <diehreo@gmail.com>
2025-02-17 17:55:53 +0900

    chore: vercel analystics

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,10 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import { Analytics } from '@vercel/analytics/react';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <>
+        <App />
+        <Analytics />
+    </>
+);
